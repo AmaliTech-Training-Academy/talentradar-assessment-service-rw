2abde38dbbf2bef9cadaaa2597baf58b156f4791
kevin <murindakevinmulk@gmail.com>
2025-07-16 11:24:07 +0200

    fix:Modify model name from UserSnapshotRepository to UserSnapshot

--- src/main/java/com/talentradar/assessment_service/repository/UserSnapshotRepository.java
+++ src/main/java/com/talentradar/assessment_service/repository/UserSnapshotRepository.java
@@ -1,10 +1,11 @@
 package com.talentradar.assessment_service.repository;
 
+import com.talentradar.assessment_service.model.UserSnapshot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface UserSnapshotRepository extends JpaRepository<UserSnapshotRepository, UUID> {
+public interface UserSnapshotRepository extends JpaRepository<UserSnapshot, UUID> {
 }
