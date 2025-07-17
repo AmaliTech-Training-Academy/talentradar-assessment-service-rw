7c2e2c54af1e482c1bc7712ff057b7438455c5b2
kevin <murindakevinmulk@gmail.com>
2025-07-17 14:03:27 +0200

    feat: Add method to validate all dimension-definition-id exists

--- src/main/java/com/talentradar/assessment_service/repository/DimensionDefinitionRepository.java
+++ src/main/java/com/talentradar/assessment_service/repository/DimensionDefinitionRepository.java
@@ -2,10 +2,15 @@ package com.talentradar.assessment_service.repository;
 
 import com.talentradar.assessment_service.model.DimensionDefinition;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
+import java.util.*;
 
 @Repository
 public interface DimensionDefinitionRepository extends JpaRepository<DimensionDefinition, UUID> {
+
+    @Query("SELECT dd.id FROM DimensionDefinition dd WHERE dd.id IN :ids")
+    List<UUID> findExistingIds(@Param("ids") List<UUID> ids); //Validate that all provided IDs exist
 }
