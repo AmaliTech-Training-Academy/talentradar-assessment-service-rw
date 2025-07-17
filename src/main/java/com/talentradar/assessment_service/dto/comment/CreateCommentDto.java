6bc2747781d94d653505b0af72566864e615a60c
ingabireol <ingabire069@gmail.com>
2025-07-17 14:32:48 +0200

    feat(tra-46): added comment dtos

---
+++ src/main/java/com/talentradar/assessment_service/dto/comment/CreateCommentDto.java
@@ -0,0 +1,16 @@
+package com.talentradar.assessment_service.dto.comment;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CreateCommentDto {
+    @NotBlank(message = "Comment title is required")
+    private String commentTitle;
+}
