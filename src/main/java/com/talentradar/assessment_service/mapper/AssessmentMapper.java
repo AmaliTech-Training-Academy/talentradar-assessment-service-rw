079dc7782acd87be7884f72b378fa61a1df60371
kevin <murindakevinmulk@gmail.com>
2025-07-18 14:40:07 +0200

    chores: Remove un used codes

--- src/main/java/com/talentradar/assessment_service/mapper/AssessmentMapper.java
+++ src/main/java/com/talentradar/assessment_service/mapper/AssessmentMapper.java
@@ -1,6 +1,5 @@
 package com.talentradar.assessment_service.mapper;
 
-import com.talentradar.assessment_service.dto.assessment.request.AssessmentRequestDTO;
 import com.talentradar.assessment_service.dto.assessment.response.AssessmentResponseDTO;
 import com.talentradar.assessment_service.dto.assessment.response.DimensionResponseDTO;
 import com.talentradar.assessment_service.model.Assessment;
@@ -14,15 +13,11 @@ import java.util.List;
 public interface AssessmentMapper {
 
     // Entity to DTO
+    @Mapping(source = "submissionStatus", target = "status")
+    @Mapping(source = "averageScore", target = "average")
     AssessmentResponseDTO toResponseDto(Assessment entity);
 
     DimensionResponseDTO toDimensionDto(AssessmentDimension dimension);
     List<DimensionResponseDTO> toDimensionDtoList(List<AssessmentDimension> dimensions);
 
-
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
-    @Mapping(target = "dimensions", ignore = true)
-    Assessment toEntity(AssessmentRequestDTO dto);
 }
