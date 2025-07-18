664569772992840ecc417c4a92b431fd72cee4ad
kevin <murindakevinmulk@gmail.com>
2025-07-18 12:17:37 +0200

    chores: Remove userId from assessment request DTO

--- src/main/java/com/talentradar/assessment_service/dto/assessment/request/AssessmentRequestDTO.java
+++ src/main/java/com/talentradar/assessment_service/dto/assessment/request/AssessmentRequestDTO.java
@@ -10,15 +10,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
-import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class AssessmentRequestDTO {
-    @NotNull
-    private UUID userId;
 
     @NotBlank
     private String reflection;
