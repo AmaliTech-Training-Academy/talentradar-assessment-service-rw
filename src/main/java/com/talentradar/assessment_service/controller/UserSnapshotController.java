d66282685d983a00d2d3be549cf8c2d4ca0c82fd
kevin <murindakevinmulk@gmail.com>
2025-07-22 11:36:39 +0200

    bugfix: Fix endpoint bug

--- src/main/java/com/talentradar/assessment_service/controller/UserSnapshotController.java
+++ src/main/java/com/talentradar/assessment_service/controller/UserSnapshotController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/v1/user-assign")
 @RequiredArgsConstructor
 public class UserSnapshotController {
 
